817c6f24ca835a626f92182843ecec240b2b3d1b
Lukas Bednar <lbednar@redhat.com>
2016-01-13 16:13:07 +0100

    Python 3 compliance
    * align with pep8
    * update metadata classifiers
    * fix issue in readme (issue-5)
    * expose attributes and parser
    
    Signed-off-by: Lukas Bednar <lbednar@redhat.com>

---
+++ otopimdp/__init__.py
@@ -0,0 +1,38 @@
+from otopimdp.parser import MachineDialogParser
+from otopimdp.parser import (
+    ABORT_KEY,
+    ATTRIBUTES_KEY,
+    REGEX_KEY,
+    TYPE_KEY,
+    REPLY_KEY,
+)
+from otopimdp.parser import (
+    CONFIRM_EVENT,
+    DISPLAY_MULTI_STRING_EVENT,
+    DISPLAY_VALUE_EVENT,
+    LOG_EVENT,
+    NOTE_EVENT,
+    QUERY_STRING_EVENT,
+    QUERY_MULTI_STRING_EVENT,
+    QUERY_VALUE_EVENT,
+    TERMINATE_EVENT,
+)
+
+
+__all__ = [
+    'MachineDialogParser',
+    'ABORT_KEY',
+    'ATTRIBUTES_KEY',
+    'REGEX_KEY',
+    'TYPE_KEY',
+    'REPLY_KEY',
+    'CONFIRM_EVENT',
+    'DISPLAY_MULTI_STRING_EVENT',
+    'DISPLAY_VALUE_EVENT',
+    'LOG_EVENT',
+    'NOTE_EVENT',
+    'QUERY_STRING_EVENT',
+    'QUERY_MULTI_STRING_EVENT',
+    'QUERY_VALUE_EVENT',
+    'TERMINATE_EVENT',
+]
